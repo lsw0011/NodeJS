29ffb2221c95c80f8239e7d53373b82b3b12169b
Luke Westerfield <lukeswesterfield@gmail.com>
2018-12-14 16:44:08 -0500

    added stripe

--- amazonlike/controllers/shop.js
+++ amazonlike/controllers/shop.js
@@ -5,7 +5,11 @@ const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
-const User = require('../models/user')
+const User = require('../models/user');
+const STRIPE_KEY = require('../data/keys').STRIPE_KEY;
+const stripe = require("stripe")(STRIPE_KEY);
+
+
 
 const ITEMS_PER_PAGE = 2;
 
@@ -122,11 +126,39 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      })
+    res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout', 
+    products: products,
+    totalSum: total
+  })
+})
+ 
+}
+
+exports.postOrder = (req, res, next) => {
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+  const token = req.body.stripeToken; // Using Express
+  let totalSum = 0;
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      user.cart.items.forEach(p => {
+        totalSum += p.quantity * p.productId.price;
+      });
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
@@ -140,6 +172,13 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
+      const charge = stripe.charges.create({
+        amount: totalSum * 100,
+        currency: 'usd',
+        description: 'Demo Order',
+        source: token,
+        metadata: { order_id: result._id.toString()}
+      });
       return req.user.clearCart();
     })
     .then(() => {
